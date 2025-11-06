775b5f05cb668d30ef587fe2bfc3db09937c0048
DAVILUIZFELTRIN <daviluizfeltrin@GMAIL.COM>
2025-11-06 12:45:36 -0300

    Delete nexus/fix_senha_empresas.sql

--- nexus/fix_senha_empresas.sql
+++
@@ -1,4 +0,0 @@
--- Corrigir o tamanho do campo senha na tabela empresas
--- O campo precisa ter 255 caracteres para armazenar hashes bcrypt corretamente
-
-ALTER TABLE `empresas` MODIFY `senha` VARCHAR(255) DEFAULT NULL;
